cd3802cd38b23b861e1d5eadf2b3e61be096ba42
jbrasko <jbrasko0508@gmail.com>
2021-07-12 15:35:39 -0500

    add image margin

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -27,6 +27,7 @@ const ProjectImage = styled.img`
   height: 240px;
   box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.2);
   transition: 0.3s;
+  margin-top: 24px;
 
   &:hover {
     box-shadow: 4px 8px 16px 4px rgba(0, 0, 0, 0.2);
