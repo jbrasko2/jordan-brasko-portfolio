5090ecbd481607ce2c247c71f09c476cd84f3bfe
jbrasko <jbrasko0508@gmail.com>
2021-07-13 15:11:40 -0500

    display images based on url

--- src/components/LocationPage.js
+++ src/components/LocationPage.js
@@ -1,21 +1,78 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import styled from 'styled-components/macro';
 import { useParams } from 'react-router-dom';
 
 const LocationPage = () => {
   const { url } = useParams();
   function importAll(r) {
-      console.log(r.keys().map(r))
     return r.keys().map(r);
   }
-  
-  const images = importAll(require.context('../location-images/california', false, /\.(png|jpe?g|svg)$/));
+
+  const locations = [
+    {
+      state: 'arizona',
+      images: importAll(
+        require.context(
+          '../location-images/arizona',
+          false,
+          /\.(png|jpe?g|svg)$/
+        )
+      ),
+    },
+    {
+      state: 'california',
+      images: importAll(
+        require.context(
+          '../location-images/california',
+          false,
+          /\.(png|jpe?g|svg)$/
+        )
+      ),
+    },
+    {
+      state: 'colorado',
+      images: importAll(
+        require.context(
+          '../location-images/colorado',
+          true,
+          /\.(png|jpe?g|svg)$/
+        )
+      ),
+    },
+    {
+      state: 'new-mexico',
+      images: importAll(
+        require.context(
+          '../location-images/new-mexico',
+          true,
+          /\.(png|jpe?g|svg)$/
+        )
+      ),
+    },
+  ];
+
+  const location = locations.find(location => location.state === url);
+
   return (
-    <>
-      {images.map(image => (
-        <img src={image.default} alt='info'/>
+    <Wrapper>
+      {location.images.map(image => (
+        <ImageCard src={image.default} alt='info' />
       ))}
-    </>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ImageCard = styled.img`
+  display: block;
+  margin: 100px;
+  max-width: 700px;
+  max-height: 700px;
+`;
 
 export default LocationPage;
